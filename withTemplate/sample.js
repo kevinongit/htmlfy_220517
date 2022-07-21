3398bdf4ef97a6f1178bbce303ec88cffc6dbc11
Kevin <nait90@gmail.com>
2022-07-21 13:59:54 +0900

    Create sample.js

---
+++ withTemplate/sample.js
@@ -0,0 +1,21 @@
+{
+  "proc_status": [
+    {
+      "issue_number": 522,
+      "project_name": "02.통합테스트",
+      "biz_type": "스타뱅킹",
+      "issue_kind": "화면오류",
+      "environment": "스테이징",
+      "subject": "[스타뱅킹] 최저금리/최대한도 영역 오류",
+      "current_status": "확인완료",
+      "priority": "Minor",
+      "author": "홍길동",
+      "current_worker": "전우치",
+      "reg_date": "2022-06-14 15:26:23",
+      "end_date": "2022-06-30 15:26:23",
+      "fix_worker": null,
+      "fix_date": null,
+    }, 
+    {}
+  ]
+}
