6fb48b05f40fd1742d136f9580f711725ce5ee3a
Kevin <nait90@gmail.com>
2022-07-27 17:55:42 +0900

    update
    bug fix and now runnable

--- withTemplate/test4.js
+++ withTemplate/test4.js
@@ -12,38 +12,24 @@ const redStatus = {
   nonError: '비결함',
 }
 
+const REDMINE_HOST = 'http://10.2.55.67'
+
+const MARK1 = '&#10004;' // &#9989;
+const MARK_CHECK = '&#10004;'
+const MARK_INIT = '&#128154;'
+
 function timestamp2() {
   const today = new Date()
   today.setHours(today.getHours() + 9)
-  return today.toISOString().replace('T', ' ').substring(5,16)
+  return today.toISOString().replace('T', ' ').substring(5, 16)
 }
 
 function getFilteredCount(tuple, filter) {
-  return filter.map(f => tuple[f].length).reduce((acc, cur) => (acc + cur), 0)
+  return filter.map(f => tuple[f].length).reduce((acc, cur) => acc + cur, 0)
 }
 
 function getFilteredRecords(tuples, filter) {
   return tuples.filter(tuple => getFilteredCount(tuple, filter))
-}
-
-function load3() {
-  http
-    .get('http://10.2.55.67/redmine/proc_status_viewer/t3.json?key=fvksds32nfdsjfksdjkfsdjfjfd', response => {
-      let data = ''
-
-      response.on('data', chunk => {
-        data += chunk
-      })
-
-      response.on('end', () => {
-        const result = JSON.parse(data)
-        console.log(result.proc_status.length)
-        process(result.proc_status)
-      })
-    })
-    .on('error', err => {
-      console.log(`Error: ${err.message}`)
-    })
 }
 
 function calculateBizDays(startDate, endDate) {
@@ -94,7 +80,7 @@ function numericInit(status) {
 
 function arrayInit(status) {
   let arrayStatus = {...status}
-  Object.keys(status).map(k => (arrayStatus[k]) = []))
+  Object.keys(status).map(k => (arrayStatus[k] = []))
   return arrayStatus
 }
 
@@ -108,6 +94,7 @@ function getSummary2(list) {
   let defectList = []
   let delayedList = []
   let improvementList = []
+  const {improvement, nonError, ...subStatus} = redStatus
 
   list.map(item => {
     const status = item['current_status']
@@ -157,7 +144,6 @@ function getSummary2(list) {
     }
     let currentList = issueIsDefect(issueKind) ? defectList : improvementList
     let assignee = currentList.find(a => a.name === name)
-    const {improvement, nonError, ...subStatus} = redStatus
     if (!assignee) {
       assignee = {
         name,
@@ -178,40 +164,41 @@ function getSummary2(list) {
     } else {
       console.log(`+ Warning: unknown status(${status})`)
     }
-    currentList = currentList.map(a => a.name === assignee.name ? assignee : a)
+    currentList = currentList.map(a => (a.name === assignee.name ? assignee : a))
   })
-  defectList = defectList.sort((a,b) => (b.open.length + b.inProgress.length + b.reopen) - (a.open.length + a.inProgress.length + a.reopen))
-  improvementList = improvementList.sort((a,b) => (b.open.length + b.inProgress.length + b.reopen) - (a.open.length + a.inProgress.length + a.reopen))
+  defectList = defectList.sort((a, b) => b.open.length + b.inProgress.length + b.reopen - (a.open.length + a.inProgress.length + a.reopen))
+  improvementList = improvementList.sort((a, b) => b.open.length + b.inProgress.length + b.reopen - (a.open.length + a.inProgress.length + a.reopen))
 
-  function getStatus(list) {
-    let total = 0;
+  function getStateStatus(list) {
+    let total = 0
     let status = {}
-    Object.keys(redStatus).map(key => {
-      status[key] = list.map(x => x[key].length).reduce((acc,cur) => (acc+cur), 0)
+    Object.keys(subStatus).map(key => {
+      status[key] = list.map(x => x[key].length).reduce((acc, cur) => acc + cur, 0)
       total += status[key]
     })
     return {
       total,
-      ...status
+      ...status,
     }
   }
-  const defectStatus = getStatus(defectList)
-  const improvementStatus = getStatus(improvementStatus)
+  const defectStatus = getStateStatus(defectList)
+  const improvementStatus = getStateStatus(improvementList)
 
   return {
     scorecard,
     defectList,
     improvementList,
-    defectStatus : getStatus(defectList),
-    improvementStatus: getStatus(improvementStatus),
+    defectStatus,
+    improvementStatus,
     delayedList,
   }
 }
 
 function getSubHeader(prefix, status) {
+  console.log({status})
   const undone = status.open + status.inProgress + status.reopen
   const done = status.total - undone
-  const percent = `${((done / status.total) * 100).toFixed(0)}`
+  const percent = !done ? 0 : `${((done / status.total) * 100).toFixed(0)}`
   return `
     <h3> ${MARK1} ${prefix === 'small' ? '개선사항' : '결함'} 조치현황
       <button class="issue1-button" title="신규,조치중,재결함" 
@@ -225,50 +212,51 @@ function getSubHeader(prefix, status) {
       <label for="progress-bar">진행율 ${percent}</label> 
       <progress id="progress-bar" value="${percent}" max="100"> ${percent} </progress>
     </h3>
-    <p class="subtitle">조치율 ${percent}% [전체: ${status.totla}, 진행중: ${undone}] </p>
+    <p class="subtitle">조치율 ${percent}% [전체: ${status.total}, 진행중: ${undone}] </p>
     <span style="color: red; font-size: 14px; font-weight: bold;"> &nbsp; ${MARK_CHECK} &nbsp; 파란색 원 클릭시 해당 결함맘ㄴ 결함링크에 표시 </span>
   `
 }
 
 function getTable(prefix, status, records) {
   const header = [
-    {key: 'name', title: 'Name', width: "10%"},
-    {key: 'open', title: '신규', width: "10%"},
-    {key: 'inProgress', title: '조치중', width: "10%"},
-    {key: 'reopen', title: '재결함', width: "10%"},
-    {key: 'inReview', title: '조치완료', width: "10%"},
-    {key: 'underReview', title: '검토완료', width: "10%"},
-    {key: 'approved', title: '확인완료', width: "10%"},
-    {key: 'issues', title: '결함링크', width: "10%"},
+    {key: 'name', title: 'Name', width: '10%'},
+    {key: 'open', title: '신규', width: '10%'},
+    {key: 'inProgress', title: '조치중', width: '10%'},
+    {key: 'reopen', title: '재결함', width: '10%'},
+    {key: 'inReview', title: '조치완료', width: '10%'},
+    {key: 'underReview', title: '검토완료', width: '10%'},
+    {key: 'approved', title: '확인완료', width: '10%'},
+    {key: 'issues', title: '결함링크', width: '30%'},
   ]
   const subHeader = getSubHeader(prefix, status)
 
+  let ths = ''
+  let cols = ''
+
+  header.forEach(hdr => {
+    cols += ` <col span="1" width="${hdr.width}" style="background-color: #FF9C4;"> `
+    ths += `<th style="text-align: center;">${hdr.title}</th>`
+  })
   let table = `
     <div class="onepiece">
       <div class="trow">
         <div class="tcards">
           <table class="guide_table">
+            <colgroup>${cols}</colgroup> 
+            <tr>${ths}</tr>
   `
-  let thHtml = ""
-  let cols = ""
-
-  header.forEach(hdr => {
-    cols += ` <col span="1" width="${hdr.width}" style="background-color: #FF9C4;"> `
-    thHtml += `<th style="text-align: center;">${hdr.title}</th>`
-  })
-  table += `<colgroup>${cols}</colgroup> <tr>${thHtml}</tr>`
 
   records.map((record, idx) => {
-    let tdHtml = ""
+    let tdHtml = ''
     header.map((hdr, jdx) => {
       const key = hdr.key
       const valId = `${prefix}_${idx}_${key}`
-      let copyButton = ""
-      let blockStyle = "td-block"
+      let copyButton = ''
+      let blockStyle = 'td-block'
       const needLink = Array.isArray(record[key]) && record[key].length && key !== 'approved'
       let text = redStatus.hasOwnProperty(key) ? record[key].length : record[key]
       const targetId = `${prefix}_$${idx}_issues`
-      const showTag = needLink ? `<button class="issue2-button" onclick="showIssue('${prefix}', '${targetId}', ${idx}, '${key}' )"> ${text} </button>` : ""
+      const showTag = needLink ? `<button class="issue2-button" onclick="showIssue('${prefix}', '${targetId}', ${idx}, '${key}' )"> ${text} </button>` : ''
       tdHtml += `
         <td style="text-align: center; word-break: break-all;">
           <div class="${blockStyle}">
@@ -298,29 +286,188 @@ function getTable(prefix, status, records) {
         </div>
       </div> <br>
   `
-  return content + table
+  return subHeader + table
 }
 
 function getDelayTable(records) {
+  const statusRedline = 4 // days
+  const statusYellowline = 1
+  const statusRedCount = records.filter(x => x.dayNum > statusRedline).length
+  const statusYellowCount = records.filter(x => x.dayNum > statusYellowline).filter(x => x.dayNum <= statusRedline).length
+  const statusNormalCount = records.length - (statusRedCount + statusYellowCount)
+  const aHeader = [
+    {title: '상태', width: '5%'},
+    {key: 'name', title: 'Name', width: '10%'},
+    {key: 'dayStr', title: '등록일', width: '10%'},
+    {key: 'issue', type: 'issueLink', title: '결함', width: '75%'},
+  ]
+  let content = `<br><br><h3>${MARK_CHECK} 지연결함 현황 (기준 2영업일 이상) </h3>
+    <p class='subtitle'>[전체: ${records.length}, 지연: ${statusRedCount}, 경고: ${statusYellowCount}, 일반: ${statusNormalCount}] </p>
+  `
 
+  let ths = ''
+  let cols = ''
+  aHeader.forEach(hdr => {
+    cols += `<col span='1' width='${hdr.width}' style='background-color: #FF9C4;'`
+    ths += `<th style='text-align: center;'>${hdr.title}</th>`
+  })
+  let table = `
+    <div class='onepiece'>
+      <div class='trow'>
+        <div class='tcards'>
+          <table class='guide_table'>
+            <colgroup> ${cols} </colgroup>
+            <tr> ${ths} </tr>
+  `
+  const header = aHeader.slice(1) // remove 1st column : title
+  records.length &&
+    records.map(aRecord => {
+      let tdHtml = ''
+      const {author, title, dayNum, isUpgrade, ...record} = aRecord
+      const statusColor = dayNum > statusRedline ? 'red' : dayNum > statusYellowline ? 'yellow' : 'aqua'
+      const statusTag = isUpgrade ? `<span class='issue-clear'>${MARK_INIT}</span>` : `<span class='status' style='background-color: ${statusColor}'></span>`
+      const status = `
+      <td style='text-align: center; word-break: break-all;'>
+        <div class='td-block'>
+          <div class='td-edit'>
+            ${statusTag}
+          </div>
+        </div>
+      </td>
+    `
+
+      header.forEach(hdr => {
+        let blockStyle = 'td-block'
+        let text = record[hdr.key]
+        if (hdr.type === 'issueLink') {
+          const atitle = author + ':' + title
+          text = `<br><a title='${atitle}' href='${REDMINE_HOST}/redmine/issues/${record[hdr.key]}' target='_blank'>#${
+            record[hdr.key]
+          } - ${atitle} &nbsp;<br></a><br>`
+        }
+        tdHtml += `
+        <td style='text-align:center; word-wrap:break-all;'>
+          <div class='${blockStyle}'>
+            <div class='td-edit'>
+              ${text}
+            </div>
+          </div>
+        </td>
+      `
+      })
+      table += `
+      <tr>
+        ${status}
+        ${tdHtml}
+      </tr>
+    `
+    })
+
+  console.log(`records.length = ${records.length}`)
+  if (records.length === 0) {
+    table += `
+      <td colspan='${aHeader.length}' style='text-align: center; word-break: break-all;'>
+        <div class='td-block'>
+          <div class='td-edit'>
+            존재하지 않습니다
+          </div>
+        </div>
+      </td>
+    `
+  }
+  table += `
+          </table>
+        </div>
+      </div>
+    </div>
+  `
+  return content + table
 }
 
-function getStatus({total, open, inProgress, reopen, improvements, noissue, }) {
+function getStatus({total, open, inProgress, reopen, improvement, nonError}) {
   const undone = open + inProgress + reopen
   const done = total - undone
-  const percent = `${((done/total)*100).toFixed(1)} %`
+  const percent = `${((done / total) * 100).toFixed(1)} %`
 
-  return `<h3>${MARK1} 요약 [${timestamp2()}]</h3><p class="subtitle"> 조치율 ${percent} [전체: ${total}, 조치: ${done}, 진행중: ${undone}] (개선사항: ${improvements}, 비결함: ${noissue} 포함)`
+  return `<h3>${MARK1} 요약 [${timestamp2()}]</h3><p class="subtitle"> 조치율 ${percent} [전체: ${total}, 조치: ${done}, 진행중: ${undone}] (개선사항: ${improvement}, 비결함: ${nonError} 포함)`
 }
 
-module.exports = function processRedstat(orgList) {
-  const {status, defectStatus, improveStatus, records, records2, delayedList} = getSummary2(orgList)
+// module.exports =
+function processRedstat(orgList) {
+  const {scorecard, defectStatus, improvementStatus, defectList, improvementList, delayedList} = getSummary2(orgList)
 
-  const bigRecords = getFilteredRecords(records, ['open','inProgress','inReview', 'underReview','reopen'])
-  const smallRecords = getFilteredRecords(records2, ['open','inProgress','inReview', 'underReview','reopen'])
-  const statusContent = getStatus(status)
+  const bigRecords = getFilteredRecords(defectList, ['open', 'inProgress', 'inReview', 'underReview', 'reopen'])
+  const smallRecords = getFilteredRecords(improvementList, ['open', 'inProgress', 'inReview', 'underReview', 'reopen'])
+  const statusContent = getStatus(scorecard)
 
   const bigContents = getTable('big', defectStatus, bigRecords)
-  const smallContents = getTable('small', improveStatus, smallRecors)
-  const contents = (bigRecords.length ? bigContents + smallContents : smallContents + bigContents) + getDelayTable(delay)
+  const smallContents = getTable('small', improvementStatus, smallRecords)
+  const contents = (bigRecords.length ? bigContents + smallContents : smallContents + bigContents) + getDelayTable(delayedList)
+
+  // const templateContent = fs.readFileSync(`D:/mylab/http2/imported/xfer/template.html`)
+  const templateContent = fs.readFileSync(`${__dirname}/template.html`)
+  let html = templateContent.toString()
+
+  html = html.replace('CONTENT_HERE', statusContent + contents)
+  html = html.replace("'BIG_RECORDS'", JSON.stringify(bigRecords))
+  html = html.replace("'SMALL_RECORDS'", JSON.stringify(smallRecords))
+
+  return html
 }
+
+function load3() {
+  http
+    .get(`${REDMINE_HOST}/redmine/proc_status_viewer/t3.json?key=fvksds32nfdsjfksdjkfsdjfjfd`, response => {
+      let data = ''
+
+      response.on('data', chunk => {
+        data += chunk
+      })
+
+      response.on('end', () => {
+        const result = JSON.parse(data)
+        console.log(result.proc_status.length)
+        const html = processRedstat(result.proc_status)
+        // console.log(html)
+      })
+    })
+    .on('error', err => {
+      console.log(`Error: ${err.message}`)
+    })
+}
+
+function loadFake() {
+  const loadJSON = file => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(file, 'utf8', (err, content) => {
+        if (err) {
+          reject(err)
+        } else {
+          try {
+            resolve(JSON.parse(content))
+          } catch (err) {
+            reject(err)
+          }
+        }
+      })
+    })
+  }
+  const file = __dirname + '/sample.json'
+  loadJSON(file)
+    .then(result => {
+      // console.log(result)
+      const html = processRedstat(result.proc_status)
+      // console.log(html)
+      const outfile = `${__dirname}/redmine_stat2.html`
+      fs.writeFile(outfile, html, 'utf8', err => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(`file ${outfile} was generated.`)
+        }
+      })
+    })
+    .catch(console.log)
+}
+
+loadFake()
