ae16fb56ce49472549738e481ca8c509619d9f96
Kevin <nait90@gmail.com>
2022-08-11 15:05:03 +0900

    Update sample.js

--- withTemplate/sample.js
+++ withTemplate/sample.js
@@ -7,7 +7,7 @@
       "issue_kind": "화면오류",
       "environment": "스테이징",
       "subject": "[스타뱅킹] 최저금리/최대한도 영역 오류",
-      "current_status": "확인완료",
+      "current_status": "조치중",
       "priority": "Minor",
       "author": "홍길동",
       "current_worker": "전우치",
@@ -15,7 +15,32 @@
       "end_date": "2022-06-30 15:26:23",
       "fix_worker": null,
       "fix_date": null,
-    }, 
-    {}
+      "history_list": [
+        {
+          "change_date": "2022-07-29 09:08:30",
+          "change_user": 66,
+          "note": "",
+          "change_item": "subject",
+          "change_before": "[스타뱅킹] 메인 Nice 연동 오류 ( 신용정보 조회)",
+          "change_after": "[스타뱅킹][스마트대출] 메인 Nice 연동 오류 ( 신용정보 조회)"
+        },
+        {
+          "change_date": "2022-07-29 09:08:54",
+          "change_user": 66,
+          "note": "",
+          "change_item": "subject",
+          "change_before": "[스타뱅킹] 메인 Nice 연동 오류 ( 신용정보 조회)",
+          "change_after": "[스타뱅킹] 메인 Nice 연동 오류 ( 신용정보 조회)"
+        },
+        {
+          "change_date": "2022-07-29 09:42:30",
+          "change_user": 18,
+          "note": null,
+          "change_item": "12",
+          "change_before": "스테이징",
+          "change_after": "운영"
+        }
+      ]
+    }
   ]
 }
